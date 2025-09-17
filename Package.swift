3f584bc5029d3c85d339401340501c16b2eba46e
narcisbadea1 <narcisbadea@gmail.com>
2025-09-17 15:29:33 +0300

    add test package

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AppnomixCommerce",
+    platforms: [.iOS(.v15)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "AppnomixCommerce",
+            targets: ["AppnomixCommerce"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AppnomixCommerce",
+            url: "https://github.com/NomixGroup/ios_commerce_sdk_binary/releases/download/2.0.0-beta03/AppnomixCommerce.xcframework.zip",
+            checksum: "e881808003cb1d6287d85e8d61e372d6c2da8c0db27b77e6fea61f2003bbbe92"
+        ),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# Appnomix Commerce SDK for iOS
+
+The integration guide is available [here](https://docs.appnomix.app/docs/commerce-ios#/).
